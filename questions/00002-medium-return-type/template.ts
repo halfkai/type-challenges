b92587ca3e0bc8027e6c46d79cbdd34ac945764c
halfkai <0x158105@gmail.com>
2022-06-17 09:37:39 +0800

    MyReturnType

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,5 @@
-type MyReturnType<T> = any
+// type MyReturnType<T> = any
+
+// type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+
+type MyReturnType<T> = T extends (...args: never) => infer R ? R : never
