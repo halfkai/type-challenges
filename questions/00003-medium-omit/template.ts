899e31ea0ffe23c55594af9b3368672daa3f6287
halfkai <0x158105@gmail.com>
2022-06-18 11:11:24 +0800

    MyOmit with impl function

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,7 @@
-type MyOmit<T, K> = any
+const myOmit = (obj: Record<string, unknown>, keys: string[]) => keys.reduce<Partial<typeof obj>>((acc, k) => {
+  if (k in obj)
+    delete acc[k]
+  return acc
+}, { ...obj })
+
+type MyOmit<T, K extends keyof T> = MyPick<T, Exclude<keyof T, K>>
