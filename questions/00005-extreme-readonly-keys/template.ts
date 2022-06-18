24b9e9e826dca4f1f4c9ab8c761b12736db7e205
halfkai <0x158105@gmail.com>
2022-06-18 13:53:22 +0800

    ReadonlyKeys

--- questions/00005-extreme-readonly-keys/template.ts
+++ questions/00005-extreme-readonly-keys/template.ts
@@ -1,1 +1,7 @@
-type GetReadonlyKeys<T> = any
+import type { Equal } from '@type-challenges/utils'
+
+type WritableKeysOf<T> = {
+  [P in keyof T]: Equal<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }> extends true ? P : never
+}[keyof T]
+
+export type GetReadonlyKeys<T> = MyExclude<keyof T, WritableKeysOf<T>>
