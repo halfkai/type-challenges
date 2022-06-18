a0447f533a98e64119cb536c1e7b2cce57ff99dc
halfkai <0x158105@gmail.com>
2022-06-18 08:48:16 +0800

    MyPick with impl function

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,9 @@
-type MyPick<T, K> = any
+const myPick = (obj: Record<string, unknown>, keys: string[]) => keys.filter(
+  k => Object.hasOwn(obj, k),
+).reduce<Partial<typeof obj>>(
+  (acc, k) => {
+    acc[k] = obj[k as keyof typeof obj]
+    return acc
+  }, {})
+
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] }
